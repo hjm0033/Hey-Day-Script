95532ed621db3f0046f375b25f4a7bcbbc452f3c
hjm0033 <hjm0033@auburn.edu>
2024-10-10 09:37:33 -0500

    Create HeyDayEC.sh

---
+++ HeyDayEC.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# User enters their name
+echo "Enter your name"
+# Entered name is read
+read name
+# The name that is input is used in the script
+echo "Hey $name, War Eagle!"
+exit 
