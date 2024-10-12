814d09af10b4c85bda2ace1c48ff004240c0357a
hjm0033 <hjm0033@auburn.edu>
2024-10-12 14:05:39 -0500

    Update HeyDayEC.sh

--- HeyDayEC.sh
+++ HeyDayEC.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
-# User enters their name
-echo "Enter your name"
+# User is welcomed to the event and enters their name
+echo "Welcome to Auburn's Hey day 2024! Please enter your name"
 # Entered name is read
 read name
 # The name that is input is used in the script
 echo "Hey $name, War Eagle!"
+#Thanks for participating
+echo Thanks for participating in Hey Day! Share your Auburn spirit by saying hey the rest of the day"
 exit 
